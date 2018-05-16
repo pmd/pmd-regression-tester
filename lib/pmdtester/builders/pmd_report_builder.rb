a7880edb97611515c5d5aa498e9a2ca47541a7ad
BBG <djydewang@gmail.com>
2018-05-17 01:35:50 +0800

    add pmd report builder

--- lib/pmdtester/builders/pmd_report_builder.rb
+++ lib/pmdtester/builders/pmd_report_builder.rb
@@ -1,36 +1,81 @@
-require "../cmd"
+require 'fileutils'
+require '../cmd'
 module PmdTester
   class PmdReportBuilder
-    def initialize(projects, local_git_repo, pmd_branch_name)
+    def initialize(branch_config, projects, local_git_repo, pmd_branch_name, rule_sets)
+      @branch_config = branch_config
       @projects = projects
+      @local_git_repo = local_git_repo
       @pmd_branch_name = pmd_branch_name
+      @rule_sets = rule_sets
+      @pwd = Dir.getwd
+    end
+
+    def create_repositories_dir
+      @repositories_dir = @pwd + "/target/repositories"
+      FileUtils.mkdir(@repositories_dir) unless File.directory?(@repositories_dir)
     end
 
     def get_projects
       puts "Cloning projects started"
 
+      create_repositories_dir
+
       @projects.each do |project|
-        path = Dir.getwd + "/target/repositories/" + project.name
+        path = @repositories_dir + project.name
         clone_cmd = project.type + " clone " + project.connection + " " + path
-        begin
-          PmdTester::Cmd.execute(clone_cmd) unless File::exist?(path)
-        rescue Exception => e
-          puts e.message
-          exit(1)
+        PmdTester::Cmd.execute(clone_cmd) unless File::exist?(path)
         end
-      end
     end
 
     def get_pmd_binary_file
-      pwd = Dir.getwd
-      Dir.chdir(local_git_repo)
+      Dir.chdir(@local_git_repo)
+
       checkout_cmd = "git checkout " + @pmd_branch_name
-      begin
-        PmdTester::Cmd.execute(checkout_cmd)
-      rescue Exception => e
-        puts e.message
-        exit(1)
+      PmdTester::Cmd.execute(checkout_cmd)
+
+      package_cmd = "./mvnw clean package -Dpmd.test.skip=true -Dpmd.skip=true"
+      PmdTester::Cmd.execute(package_cmd)
+
+      version_cmd = "./mvnw -q -Dexec.executable=\"echo\" -Dexec.args='${project.version}' " +
+          "--non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec | tail -1"
+      @pmd_version, errors = PmdTester::Cmd.execute(version_cmd)
+
+      target_dir = @pwd + "/target"
+      unzip_cmd = "unzip -qo pmd-dist/target/pmd-bin-#{@pmd_version}.zip -d #{target_dir}"
+      PmdTester::Cmd.execute(unzip_cmd)
+
+      Dir.chdir(@pwd)
+    end
+
+    def generate_pmd_report(src_root_dir, report_file)
+      run_path = "target/pmd-bin-#{@pmd_version}/bin/run.sh"
+      pmd_cmd = run_path + " -d " + src_root_dir + " -f xml -R" +  @rule_sets + " -r " + report_file
+      PmdTester::Cmd.execute(pmd_cmd)
+      if $?.to_i == 1
+        puts "Error! Generating pmd report failed"
+        exit 1
       end
+    end
+
+    def generate_pmd_reports
+      puts "Generating pmd Report started -- branch #{@branch_name}"
+
+      get_pmd_binary_file
+
+      branch_file = "target/reports/" + @pmd_branch_name.delete("/")
+      FileUtils::mkdir(branch_file) unless File.directory?(branch_file)
+
+      @projects.each do |project|
+        project_report_file = branch_file + "/" + project.name + ".xml"
+        project_source_dir = "target/repositories/" + project.name
+        generate_pmd_report(project_source_dir, project_report_file)
+      end
+    end
+
+    def build
+      get_projects
+      generate_pmd_reports
     end
   end
 end
--- lib/pmdtester/cmd.rb
+++ lib/pmdtester/cmd.rb
@@ -4,13 +4,16 @@ module PmdTester
   class Cmd
     def self.execute(cmd)
       puts cmd
+
+      begin
       stdin, stdout, stderr = Open3.popen3(cmd)
-
-      raise Exception, "Execute #{cmd} failed!" if $?.to_i == 1
-
       result = stdout.gets
       result.chomp! unless result.nil?
       [result, stderr.gets]
+      rescue Exception => e
+        puts e.message
+        exit(1)
+      end
     end
   end
 end
