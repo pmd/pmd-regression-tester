58b7b67476f4a302bfb56bfc13b843574e19ce3b
Andreas Dangel <andreas.dangel@adangel.org>
2018-09-02 21:32:51 +0200

    Update integration test to use PMD 6.7.0 and master

--- test/integration_test_runner.rb
+++ test/integration_test_runner.rb
@@ -9,17 +9,17 @@ class IntegrationTestRunner < Test::Unit::TestCase
 
   def test_local_mode
     argv = '-r target/repositories/pmd -b master -bc config/design.xml' \
-              ' -p pmd_releases/6.1.0 -pc config/design.xml -l test/resources/project-test.xml'
+              ' -p pmd_releases/6.7.0 -pc config/design.xml -l test/resources/project-test.xml'
 
     system("bundle exec bin/pmdtester #{argv}")
 
     assert_equal(0, $CHILD_STATUS.exitstatus)
     assert_path_exist('target/reports/master/checkstyle/pmd_report.xml')
     assert_path_exist('target/reports/master/pmd/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.1.0/checkstyle/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.1.0/checkstyle/config.xml')
-    assert_path_exist('target/reports/pmd_releases_6.1.0/pmd/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.1.0/pmd/config.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/checkstyle/pmd_report.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/checkstyle/config.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/pmd/pmd_report.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/pmd/config.xml')
     assert_path_exist('target/reports/diff/checkstyle/index.html')
     assert_path_exist('target/reports/diff/pmd/index.html')
     assert_path_exist('target/reports/diff/index.html')
@@ -27,14 +27,14 @@ class IntegrationTestRunner < Test::Unit::TestCase
 
   def test_single_mode
     argv = '-r target/repositories/pmd -m single' \
-              ' -p pmd_releases/6.1.0 -pc config/design.xml -l test/resources/project-test.xml'
+              ' -p pmd_releases/6.7.0 -pc config/design.xml -l test/resources/project-test.xml'
 
     system("bundle exec bin/pmdtester #{argv}")
 
     assert_equal(0, $CHILD_STATUS.exitstatus)
-    assert_path_exist('target/reports/pmd_releases_6.1.0/checkstyle/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.1.0/pmd/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.1.0/pmd/config.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/checkstyle/pmd_report.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/pmd/pmd_report.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/pmd/config.xml')
     assert_path_exist('target/reports/diff/checkstyle/index.html')
     assert_path_exist('target/reports/diff/pmd/index.html')
     assert_path_exist('target/reports/diff/index.html')
@@ -42,7 +42,7 @@ class IntegrationTestRunner < Test::Unit::TestCase
 
   def test_online_mode
     base_branch = 'test_branch_2'
-    argv = "-r target/repositories/pmd -m online -b #{base_branch} -p pmd_releases/6.3.0"
+    argv = "-r target/repositories/pmd -m online -b #{base_branch} -p pmd_releases/6.7.0"
     # This test depends on the file test_branch-baseline.zip being available on sourceforge.
 
     system("bundle exec bin/pmdtester #{argv}")
@@ -50,10 +50,10 @@ class IntegrationTestRunner < Test::Unit::TestCase
     assert_path_exist("target/reports/#{base_branch}-baseline.zip")
     assert_path_exist("target/reports/#{base_branch}/checkstyle/pmd_report.xml")
     assert_path_exist("target/reports/#{base_branch}/spring-framework/pmd_report.xml")
-    assert_path_exist('target/reports/pmd_releases_6.3.0/checkstyle/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.3.0/checkstyle/config.xml')
-    assert_path_exist('target/reports/pmd_releases_6.3.0/spring-framework/pmd_report.xml')
-    assert_path_exist('target/reports/pmd_releases_6.3.0/spring-framework/config.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/checkstyle/pmd_report.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/checkstyle/config.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/spring-framework/pmd_report.xml')
+    assert_path_exist('target/reports/pmd_releases_6.7.0/spring-framework/config.xml')
     assert_path_exist('target/reports/diff/checkstyle/index.html')
     assert_path_exist('target/reports/diff/spring-framework/index.html')
     assert_path_exist('target/reports/diff/index.html')
