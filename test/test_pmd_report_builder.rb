077d81dddb4138966ea7ca5d587f27ea87665c41
Clément Fournier <clement.fournier76@gmail.com>
2020-09-29 16:27:52 +0200

    Fix tests

--- test/test_pmd_report_builder.rb
+++ test/test_pmd_report_builder.rb
@@ -26,10 +26,12 @@ class TestPmdReportBuilder < Test::Unit::TestCase
 
     # File does not exist yet this time...
     record_expectations('sha1abc', 'sha1abc', false)
-    PmdTester::Cmd.stubs(:execute).with('git checkout master')
-                  .returns('checked out branch master').once
     PmdTester::Cmd.stubs(:execute).with('./mvnw clean package -Dmaven.test.skip=true' \
                   ' -Dmaven.javadoc.skip=true -Dmaven.source.skip=true').once
+    PmdTester::Cmd.stubs(:execute).with('unzip -qo pmd-dist/target/pmd-bin-6.10.0-SNAPSHOT.zip' \
+                  ' -d pmd-dist/target/exploded').once
+    PmdTester::Cmd.stubs(:execute).with('mv pmd-dist/target/exploded/pmd-bin-6.10.0-SNAPSHOT' \
+                  ' current-dir/target/pmd-bin-sha1abc').once
     record_expectations_after_build
 
     PmdTester::PmdReportBuilder
@@ -40,22 +42,25 @@ class TestPmdReportBuilder < Test::Unit::TestCase
   def record_expectations(sha1_head, sha1_base, zip_file_exists)
     Dir.stubs(:getwd).returns('current-dir').once
     Dir.stubs(:chdir).with('target/repositories/pmd').yields.once
-    PmdTester::Cmd.stubs(:execute).with('git rev-parse HEAD').returns(sha1_head).once
     PmdTester::Cmd.stubs(:execute).with('git rev-parse master').returns(sha1_base).once
+    # inside checkout_build_branch
+    PmdTester::Cmd.stubs(:execute).with('git checkout master')
+        .returns('checked out branch master').once
     PmdTester::Cmd.stubs(:execute).with('./mvnw -q -Dexec.executable="echo" ' \
                   "-Dexec.args='${project.version}' " \
                   '--non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec')
                   .returns('6.10.0-SNAPSHOT').at_least(1).at_most(2)
+    PmdTester::Cmd.stubs(:execute).with('git status --porcelain').returns('').once
+
+    # back into get_pmd_binary_file
+    PmdTester::Cmd.stubs(:execute).with('git rev-parse HEAD').returns(sha1_head).once
     # File does not exist yet this time...
-    File.stubs(:exist?).with('pmd-dist/target/pmd-bin-6.10.0-SNAPSHOT.zip')
+    File.stubs(:directory?).with("current-dir/target/pmd-bin-#{sha1_base}")
         .returns(zip_file_exists).once
   end
 
   def record_expectations_after_build
-    PmdTester::Cmd.stubs(:execute).with('git rev-parse HEAD').returns('sha1abc').once
     PmdTester::Cmd.stubs(:execute).with('git log -1 --pretty=%B').returns('the commit message').once
-    PmdTester::Cmd.stubs(:execute).with('unzip -qo pmd-dist/target/pmd-bin-6.10.0-SNAPSHOT.zip' \
-                  ' -d current-dir/target').once
     PmdTester::PmdBranchDetail.any_instance.stubs(:save).once
     FileUtils.stubs(:cp).with('config/design.xml', 'target/reports/master/config.xml').once
   end
