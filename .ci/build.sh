9c2e1fd2132d4ba618f3b15b4e1152284d75145a
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-11-29 10:25:23 +0100

    Bump build-tools from master to 17

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,8 +9,8 @@ source "$(dirname "$0")/inc/fetch_ci_scripts.bash" && fetch_ci_scripts
 
 function build() {
     pmd_ci_log_group_start "Install OpenJDK 8+11"
-        pmd_ci_openjdk_install_adoptopenjdk 11
-        pmd_ci_openjdk_install_adoptopenjdk 8
+        pmd_ci_openjdk_install_adoptium 11
+        pmd_ci_openjdk_install_adoptium 8
         pmd_ci_openjdk_setdefault 11
     pmd_ci_log_group_end
 
