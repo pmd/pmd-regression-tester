5370a19ce0b02fd09aeb61a7b3aa6582ff6c8f28
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-05-12 11:05:49 +0200

    Fix integration test

--- test/integration_test_project_builder.rb
+++ test/integration_test_project_builder.rb
@@ -6,6 +6,8 @@ require 'etc'
 # Integration test for PmdTester::ProjectBuilder
 class IntegrationTestProjectBuilder < Test::Unit::TestCase
   include PmdTester
+  include TestUtils
+
   def setup
     `rake clean`
   end
@@ -15,11 +17,6 @@ class IntegrationTestProjectBuilder < Test::Unit::TestCase
                                                    'project-list_commit_sha1.xml')
     project_builder = PmdTester::ProjectBuilder.new(projects)
     project_builder.clone_projects
-
-    expect_git_clone('Schedul-o-matic-9000', 'https://github.com/SalesforceLabs/Schedul-o-matic-9000',
-                     '6b1229ba43b38931fbbab5924bc9b9611d19a786')
-    expect_git_clone('fflib-apex-common', 'https://github.com/apex-enterprise-patterns/fflib-apex-common',
-                     '7e0891efb86d23de62811af56d87d0959082a322')
 
     assert_path_exist('target/repositories/Schedul-o-matic-9000/.git/HEAD')
     assert_file_content_equals('6b1229ba43b38931fbbab5924bc9b9611d19a786',
